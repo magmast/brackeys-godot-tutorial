771f548d7ed8b3f66470ca1bf06c6129c1719340
Maciej Augustyniak <maciej.f.august@gmail.com>
2024-05-19 11:20:29 +0200

    feat: killable slime
    Slime can now be killed by jumping on it's head.

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,17 +1,34 @@
 extends Node2D
 
 
-const SPEED: float = 60
-
-@onready var sprite = $AnimatedSprite2D
-@onready var ray_cast_right: RayCast2D = $RayCastRight
-@onready var ray_cast_left: RayCast2D = $RayCastLeft
+const SPEED := 60.0
+const GRAVITY := 100.0
 
 
-var direction: float = 1
+@onready var sprite := $AnimatedSprite2D
+@onready var ray_cast_right := $RayCastRight
+@onready var ray_cast_left := $RayCastLeft
+@onready var ray_cast_top := $RayCastTop
+@onready var kill_zone := $KillZone
+
+
+var direction := 1.0
+var dying := false
 
 
 func _process(delta: float) -> void:
+	if dying:
+		position.y += delta * GRAVITY
+		if position.y > 500:
+			queue_free()
+		return
+	
+	if ray_cast_top.is_colliding():
+		dying = true
+		sprite.play('die')
+		kill_zone.queue_free()
+		return
+	
 	if ray_cast_right.is_colliding():
 		direction = -1
 		sprite.flip_h = true
